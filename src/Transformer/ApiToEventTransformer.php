c565c0d64519f90a392815e50975f9997e23fbb8
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2025-06-10 12:02:33 +0200

    First unit tests, exceptions

--- src/Transformer/ApiToEventTransformer.php
+++ src/Transformer/ApiToEventTransformer.php
@@ -6,8 +6,20 @@ use App\Entity\Event;
 
 class ApiToEventTransformer implements ApiToEntityTransformerInterface
 {
+    private const KEYS = [
+        'name',
+        'description',
+        'accessible',
+        'startDate',
+        'endDate',
+    ];
+
     public function transform(array $data): Event
     {
+        if (0 < \count(\array_diff(self::KEYS, \array_keys($data)))) {
+            throw new \RuntimeException("Missing keys in data provided by the API");
+        }
+
         return (new Event())
             ->setName($data['name'])
             ->setStartAt(new \DateTimeImmutable($data['startDate']))
